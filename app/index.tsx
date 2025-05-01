454c29ee2716f5bb853cca902f9b3f39ecbcc58f
LeonNjoroge <leonmuraguri@gmail.com>
2025-05-01 08:52:48 +0300

    Feat: Get details from the route param

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,14 @@
 import { Text, View } from "react-native";
+import {Link} from "expo-router";
+
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
+        <Link href="/onboarding">OnBoarding</Link>
+        <Link href="/movie/avengers">Avenger Movie</Link>
     </View>
   );
 }
+
