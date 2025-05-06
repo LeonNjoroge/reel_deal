724b6da1305742df7aaaa837cafc9c80b4939098
LeonNjoroge <leonmuraguri@gmail.com>
2025-05-06 10:44:17 +0300

    Feat: Update Movie card component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,23 @@
-import {Image, ScrollView, Text, View} from "react-native";
+import {ActivityIndicator, FlatList, Image, ScrollView, Text, View} from "react-native";
 import {images} from "@/constants/images";
 import {icons} from "@/constants/icons";
 import SearchBar from "@/components/searchBar";
 import {useRouter} from "expo-router";
+import useFetch from "@/services/useFetch";
+import {fetchMovies} from "@/services/api";
+import MovieCard from "@/components/movieCard";
 
 
 export default function Index() {
    const router = useRouter();
+
+   const {
+       data: movies,
+       loading: moviesLoading,
+       error: moviesError
+   } = useFetch(() => fetchMovies({
+       query: ''
+   }))
 
   return (
     <View
@@ -27,16 +38,52 @@ export default function Index() {
             <Image
                 source={icons.logo}
                 className="w-12 h-10 mt-20 mb05 mx-auto"
-                />
+            />
 
-            <View
+            {moviesLoading ? (
+                <ActivityIndicator
+                    size="large"
+                    color="#0000FF"
+                    className="mt-10 self-center"
+                />
+            ) : moviesError ? (
+                <Text>Error: {moviesError?.message}</Text>
+            ): (
+                <View
                 className="flex-1 mt-5"
-                >
+            >
                 <SearchBar
                     onPress={()=> router.push("/search")}
                     placeholder="Search for a Movie"
                 />
+                    <>
+                        <Text
+                            className="text-lg text-white font-bold mt-5 mb-3"
+                            >Latest Movies</Text>
+
+                        <FlatList
+                            data={movies}
+                            renderItem={({item})=> (
+                                <MovieCard
+                                    {...item}
+                                    />
+                                )}
+                            keyExtractor={(item)=> item.id.toString()}
+                            numColumns={3}
+                            columnWrapperStyle={{
+                                    justifyContent: 'flex-start',
+                                    gap: 20,
+                                    paddingRight: 5,
+                                    marginBottom: 10
+                                }}
+                            className="mt-2 pb-32"
+                            scrollEnabled={false}
+                        />
+                    </>
             </View>
+            )
+            }
+
         </ScrollView>
     </View>
   );
--- services/api.ts
+++ services/api.ts
@@ -24,7 +24,6 @@ export const fetchMovies = async ({query}: {query:string}) => {
     }
 
     const data = await response.json();
-
     return data.results;
 }
 
